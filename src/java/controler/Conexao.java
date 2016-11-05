36200301faa3d55b6d4832d9ecdc2d5caed00ea7
marinadezordi <marina@10.0.0.106>
2016-11-05 11:04:27 -0200

    pacote com.rest

--- src/java/controler/Conexao.java
+++ src/java/controler/Conexao.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package bancoDados;
+package controler;
 
 import static java.lang.Character.UnicodeBlock.forName;
 import java.sql.Connection;
--- src/java/controler/Operacoes.java
+++ src/java/controler/Operacoes.java
@@ -3,9 +3,9 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package bancoDados;
+package controler;
 
-import construtor.BelasMensagens;
+import model.BelasMensagens;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
